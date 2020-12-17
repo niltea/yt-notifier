bea3d9aeec598836b794a0a2b2a8c1d91fcb4b95
Yusuke Kishi <kishi@balcolony.com>
2020-12-17 17:09:16 +0900

    Fleetいったん削除
    Revert "debug"
    
    This reverts commit 44ab697e8f59b2c9effa3b48888ce3b7a2d44398.
    
    Revert "フリート検知機能追加"
    
    This reverts commit 82c9443cabc0d234d10f8b4243fe10e872b004f2.

--- functions/index.js
+++ functions/index.js
@@ -71,9 +71,6 @@ const readFireStore = async (id) => {
             toDate: () => { return new Date('2020/1/1 0:00'); }
           },
           latestLiveID: 'xxxx',
-          latestFleetDate: {
-            toDate: () => { return new Date('2020/1/1 0:00'); }
-          },
         },
       };
     }
@@ -104,70 +101,6 @@ const setFireStore = async (id, data) => {
     };
   }
 }
-
-const createFleetLineMessage = (NewArrival) => {
-  const payload = [];
-  NewArrival.forEach((item) => {
-    payload.push({
-      title: '新しいフリートだよ',
-      data: {
-        "type": "bubble",
-        "hero": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "新しいフリートだよ",
-              "size": "xl",
-              "weight": "bold",
-              "align": "center",
-            }
-          ],
-          "paddingAll": "md",
-          "paddingTop": "xl",
-          "paddingBottom": "lg"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "image",
-              "url": item.media_url,
-              "size": "full",
-              "aspectMode": "cover",
-              "aspectRatio": "9:16"
-            },
-          ],
-          "paddingAll": "none"
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "style": "link",
-              "height": "sm",
-              "action": {
-                "type": "uri",
-                "label": "リンク ＞",
-                "uri": item.media_url
-              },
-              "color": "#ffffff"
-            }
-          ],
-          "flex": 0,
-          "paddingAll": "none",
-          "backgroundColor": "#17c950"
-        }
-      },
-    });
-  });
-  return payload;
-};
 
 const createLineMessage = (NewArrival) => {
   const payload = [];
@@ -283,7 +216,7 @@ const tweetMessage = async (payload) => {
   }));
 };
 
-const broadcastLineMessage = async (payload, altText) => {
+const broadcastLineMessage = async (payload) => {
   const client = new line.Client({
     channelSecret: credential.LINE_SECRET,
     channelAccessToken: credential.LINE_TOKEN,
@@ -293,7 +226,7 @@ const broadcastLineMessage = async (payload, altText) => {
     try {
       await client.broadcast({
         "type": "flex",
-        "altText": altText,
+        "altText": `新しい枠ができたよ〜ん ⇒${message.title}`,
         "contents": message.data
       })
     } catch (err) {
@@ -301,42 +234,8 @@ const broadcastLineMessage = async (payload, altText) => {
     }
   }));
 }
-
-const getFleet = async () => {
-  const config = {
-    consumer_key: credential.TW_APIKEY_2,
-    consumer_secret: credential.TW_APISECRET_2,
-    access_token_key: credential.TW_TOKEN_2,
-    access_token_secret: credential.TW_SECRET_2,
-  };
-  const twFleet = new Twitter(config);
-
-  try {
-    return await twFleet.get('https://api.twitter.com/fleets/v1/user_fleets', {user_id: credential.TW_FLEET_ID});
-  } catch (error) {
-    console.log(error);
-    return {
-      isSuccess: false,
-      result: error,
-    };
-  }
-};
 // exports.main = functions.region('asia-northeast1').https.onRequest(async (request, response) => {
 exports.main = functions.region('asia-northeast1').pubsub.schedule('every 1 minutes').onRun(async (request, response) => {
-  let writeDBFlag = false;
-
-  // fleet取得する
-  const fleetResponse = await getFleet();
-  const fleetThreads = fleetResponse['fleet_threads'];
-  let fleets = [];
-  if (fleetThreads.length !== 0) {
-    fleetThreads[0]['fleets'].forEach((fleet) => {
-      fleets.push({
-        created_at: new Date(fleet.created_at),
-        media_url: fleet.media_entity.media_url_https,
-      });
-    });
-  }
   // YouTubeから配信情報を取得
   const liveID = await getUpcomingLiveID();
   if (liveID.isSuccess === false) {
@@ -352,55 +251,41 @@ exports.main = functions.region('asia-northeast1').pubsub.schedule('every 1 minu
     response.send('failed to read FireStore');
     return;
   }
-  const setFireStoreSetData = liveInfoFromStore.result;
-  const latestFleetDate = liveInfoFromStore.result.latestFleetDate.toDate() || new Date('2020/01/01');
-
-  if (fleets.length && fleets[0].created_at > latestFleetDate) {
-    const LinePayload = createFleetLineMessage([fleets[0]]);
-    response.send('debug……');
-    await broadcastLineMessage(LinePayload, '新しいフリートがあるよ');
-    writeDBFlag = true;
-    setFireStoreSetData.latestFleetDate = fleets[0].created_at;
-  }
-
   const latestLiveIDFromStore = liveInfoFromStore.result.latestLiveID;
-  if ((latestLiveIDFromStore === liveID.result) && !writeDBFlag) {
+  if (latestLiveIDFromStore === liveID.result) {
     // 前回と同じ配信枠だったのでなにもせず終了
-    console.log('前回と同じ配信枠だったしFleetの新着もないな……')
-    response.send('前回と同じ配信枠だったしFleetの新着もないな……');
+    response.send('前回と同じ配信枠だったな……');
     return;
-  } else if (latestLiveIDFromStore !== liveID.result) {
-    // 配信情報の取得
-    const liveFromID = await getUpcomingLiveInfo(liveID.result);
-    if (liveFromID.isSuccess === false) {
-      // Fetch失敗時の処理
-      response.send('配信情報の取得に失敗したっぽい');
-      return;
-    } else {
-      const latestTimeFromStore = liveInfoFromStore.result.latestDate.toDate();
-      if (latestTimeFromStore > liveFromID.result.time) {
-        // 以前の配信枠より古いのでなにもせず終了
-        response.send('たぶんフリーチャット引っ掛けたな……');
-        return;
-      }
-      const LinePayload = createLineMessage([liveFromID.result]);
-      await broadcastLineMessage(LinePayload, `新しい枠ができたよ〜ん ⇒${liveFromID.result.Title}`);
-      const TwitterPayload = createTwitterMessage([liveFromID.result]);
-      await tweetMessage(TwitterPayload);
-      writeDBFlag = true;
-      setFireStoreSetData.latestDate = liveFromID.result.time;
-      setFireStoreSetData.latestLiveID = liveID.result;
-    }
   }
+
+  // 配信情報の取得
+  const liveFromID = await getUpcomingLiveInfo(liveID.result);
+  if (liveFromID.isSuccess === false) {
+    // Fetch失敗時の処理
+    response.send('配信情報の取得に失敗したっぽい');
+    return;
+  }
+
+  const latestTimeFromStore = liveInfoFromStore.result.latestDate.toDate();
+  if (latestTimeFromStore > liveFromID.result.time) {
+    // 以前の配信枠より古いのでなにもせず終了
+    response.send('たぶんフリーチャット引っ掛けたな……');
+    return;
+  }
+  const LinePayload = createLineMessage([liveFromID.result]);
+  await broadcastLineMessage(LinePayload);
+  const TwitterPayload = createTwitterMessage([liveFromID.result]);
+  await tweetMessage(TwitterPayload);
 
   // DBに情報書き込む
-  if (writeDBFlag) {
-    const setFireStoreResult = await setFireStore('liveInfo', setFireStoreSetData);
-    if (setFireStoreResult.isSuccess === false) {
-      // set失敗時の処理
-      response.send('failed to save FireStore');
-      return;
-    }
+  const setFireStoreResult = await setFireStore('liveInfo', {
+    latestDate: liveFromID.result.time,
+    latestLiveID: liveID.result,
+  });
+  if (setFireStoreResult.isSuccess === false) {
+    // set失敗時の処理
+    response.send('failed to save FireStore');
+    return;
   }
 
   response.send("OK!");
